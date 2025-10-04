e932fe8bf22d16b4ecf013100347cccc529412c1
shubhamchauhan639 <shubhamchauhan12196@gmail.com>
2025-10-04 22:21:20 +0530

    password generator

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useCallback, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [length, setlength] = useState(8)
+  const [numberAllowed , setnumberAllowed] = useState(false)
+  const [useCharacter , setChar] = useState(false)
+const [password , setPassword ] = useState ("")
+const passwordGenerator = useCallback(()=> {
+  let pass = ""
+  let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  if (numberAllowed) str+= "0123456789"
+  if(useCharacter) str+= "!@#$%^&*()"
+for (let i = 1; i < length; i++) {
+ let char = Math.floor(Math.random() * str.length + 1)
+ pass = str.charAt(char)
+}
+},[length,numberAllowed,useCharacter,setPassword])
+  return (
+    <>
+  <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+  <h1 className="text-white text-center my-3">Password generator</h1>
+  <div className="flex shadow rounded-lg overflow-hidden mb-4 bg-white">
+    <input 
+      type="text" 
+      value={password} 
+      className="outline-none w-full py-1 px-3 placeholder-black" 
+      placeholder="Password" 
+      readOnly 
+    />
+    <button className='outline-none bg-green-500 text-white px-3 py-0.5 shrink-0'>copy</button>
+  </div>
+  <div className='flex-text-sm gap-x-2'>
+    <div className='flex items-center gap-x-1'>
+      <input type="range"
+      min={6}
+      max={50}
+      value={length}  
+      className='cursor-pointer green-600'  
+      onChange={(e)=>{setlength(e.target.value)}}  />
+      <label> Length : {length}</label>
+    </div>
+  </div>
+</div>
+
+    </>
+  )
+}
+
+export default App
